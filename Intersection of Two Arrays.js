64d529a6312b5465bec75c7d9f5a815de353f2e2
Shubham Singh <77406420+Shubhamjatara@users.noreply.github.com>
2023-11-07 22:05:47 +0530

    Create Intersection of Two Arrays.js

---
+++ Intersection of Two Arrays.js
@@ -0,0 +1,11 @@
+var intersection = function(nums1, nums2) {
+const result =[];
+
+for(let i=0;i<nums1.length;i++)
+{
+    if(nums2.includes(nums1[i]))
+    result.push(nums1[i])
+}
+
+return [...new Set([...result])];
+};
